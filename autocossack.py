5b70975ccb8c0dcb0340166b380acbb8944a1e80
Krystian <matematyk45@o2.pl>
2024-05-13 02:40:32 +0200

    Project NOT abandoned yet - a lot of time spent on figuruing stuff out like setting up mysql server and learning a few libraries that will come in handy. Slight changes to commands.py, adding commands via chat almost works

--- autocossack.py
+++ autocossack.py
@@ -4,20 +4,18 @@ The main file of the bot, i have no idea what i'm doing
 TO DO:
 
 -clean up documentation and HTTP requests since they aren't readable at all
--get a bot to post something (need a new token, don't know what access scopes i'll need yet but i'll probably need to get a new one like 5-10 times during development)
 -set up actual commands
-
-in the far future
-- error handling
-- mysql/sqlite integration (probably both)
--
+-set up commands that post something every once in a while
+-mysql/sqlite integration (probably both)
+-error handling
+-bot should probably be able to work on many streams at once and that's not the case for now
 """
 import commands
-import requests
 import requests as rq
 import websocket as ws
 import json
 import authconfig
+import threading
 
 def get_user_id(user: str) -> str:
     """
@@ -28,6 +26,24 @@ def get_user_id(user: str) -> str:
     userurl = 'https://api.twitch.tv/helix/users?login={}'.format(user)
     userdata = rq.get(userurl, headers={'Client-Id': authconfig.client_id, 'Authorization': 'Bearer {}'.format(authconfig.app_token)}).text
     return json.loads(userdata)['data'][0]['id']
+
+def send_message(channel: str, message: str):
+    """
+    Sends a message to a twitch channel's chat
+    :param channel: the channel name you wish to send a message to
+    :param message: the message you wish to send
+    :return: None
+    """
+    rq.post('https://api.twitch.tv/helix/chat/messages',
+                  headers={ 'Client-Id': authconfig.client_id,
+                            'Authorization': 'Bearer {}'.format(authconfig.user_token),
+                            'Content-type': 'application/json'},
+                  data=json.dumps({
+                        'broadcaster_id': '{}'.format(get_user_id(channel)),
+                        'sender_id': '{}'.format(get_user_id(authconfig.username)),
+                        'message': '{}'.format(message)}))
+
+
 def _onmessage(wsapp, message):
     """
     Function reacts to websocket messages that occur during connection.
@@ -38,45 +54,42 @@ def _onmessage(wsapp, message):
     if json.loads(message)['metadata']['message_type'] == 'notification':
 
         if json.loads(message)['payload']['event']['message']['text'].startswith('!counter'):
-             print(rq.post('https://api.twitch.tv/helix/chat/messages',headers={'Client-Id': authconfig.client_id,
-                              'Authorization': 'Bearer {}'.format(authconfig.user_token),
-                              'Content-type': 'application/json'}, data=json.dumps({
-                 'broadcaster_id': '{}'.format(get_user_id(authconfig.username)),
-                 'sender_id': '{}'.format(get_user_id(authconfig.username)),
-                 'message': '{}'.format(commands.counter())
-             })))
+             send_message(authconfig.channel, commands.counter())
+
 
     if json.loads(message)['metadata']['message_type'] == 'session_keepalive':
         return
+
     if json.loads(message)['metadata']['message_type'] == 'session_welcome':
         sessionid = json.loads(message)['payload']['session']['id']
         dataforrequest = json.dumps({
             'type': 'channel.chat.message',
             'version': '1',
             "condition": {
-                "broadcaster_user_id": get_user_id(authconfig.username),
+                "broadcaster_user_id": get_user_id(authconfig.channel),
                 "user_id": get_user_id(authconfig.username)},
             'transport': {
                 "method": "websocket",
-                "session_id": sessionid}
-        })
-        a = rq.post('https://api.twitch.tv/helix/eventsub/subscriptions',
+                "session_id": sessionid}})
+
+        rq.post('https://api.twitch.tv/helix/eventsub/subscriptions',
                     headers={'Client-Id': authconfig.client_id,
                              'Authorization': 'Bearer {}'.format(authconfig.user_token),
                              'Content-type': 'application/json'}, data=dataforrequest)
-        print(a)
-    print(json.loads(message))
+
+        send_message(authconfig.channel, "AutoKozak is here!")
 
 
 
-requests.Response
+
+
 
 ws.enableTrace(True)
 
 authheaders = {'Client-Id': authconfig.client_id, 'Authorization': 'Bearer {}'.format(authconfig.app_token)}
 socket = ws.WebSocketApp("wss://eventsub.wss.twitch.tv/ws", header=authheaders, on_message=_onmessage)
 
-sc = socket.run_forever(sslopt={'username':authconfig.username, 'password':'oauth:{}'.format(authconfig.app_token),'channels':authconfig.username})
+sc = socket.run_forever(sslopt={'username':authconfig.username, 'password':'oauth:{}'.format(authconfig.app_token),'channels':authconfig.channel})
 
 
 
--- commands.py
+++ commands.py
@@ -1,49 +1,96 @@
 """
 Place where I'll hopefully be able to store bot commands
 
-Not sure wether I want to continue using commands as a class as it's messy.
+Not sure whether I want to continue using commands as a class as it's messy.
 Need to rethink how I want to manage it in the best way possible.
 
 TO DO:
--command counters can't get reset on restarting the bot (possibly store values/counters in external file)
--multiple channels should have the same command but different counts (class, but defined in a better way?)
+-command counters can't get reset on restarting the bot (database?)
+-multiple channels should have the same command but different counts (database?)
 
 """
 from typing import *
 
 
 class Command:
-    def _counter(self):
-        ret = self.count
-        self.count += 1
-        return "The count is {}".format(ret)
-    def __init__(self,
-        user_ids: list[str],
-        name: str,
-        on_call: object,
-        count: Union[int, None] = None,
-        ran_range: Union[list[int, int], None] = None,
-        *params):
+    @staticmethod
+    def add(message: str) -> str:
         """
+        A command to add another commands in chat. It's ugly, i hate it, I'm not even sure it works
+        :param message: The message from chat the bot will receive
+        :return: a string saying weather a command was successfully created
+        """
+        #I know the implementation is horrible, not-scalable etc but I want to get this to work asap
+        params = message.split()
+        name = params[1]
+        message = None
+        function = None
+        count = None
+        ran_range = None
+        for i in params[2:]:
+            if i.startswith('message='):
+                message = i[8:]
+            elif i.startswith('function='):
+                function = i[9:]
+            elif i.startswith('count='):
+                count = int(i[6:])
+            elif i.startswith('ran_range='):
+                 ran_range = i[10:]
+        try:
+            Command(name, message, function, count, ran_range)
+            return "Command added."
+        except: #bare except block yada yada
+            return "Error - could not add command."
 
-        :param user_ids: list of user ID's for which the command should be accessible
+
+    @staticmethod
+    def edit(message):
+        pass
+
+    commands = {'add':add, "edit":edit}
+    def __init__(self, name: str, message: Optional[str] = None, function: Optional[str] = None, count: Optional[int] = None, ran_range: Optional[list[int,int]] = None, *params):
+        """
+        :param name: the name of the command, maybe it'll come in handy, dunno yet
+        :param message: message to be sent out to chat
+        :param function: what the function does (for example increase self.counter, ban a person etc)
         :param count: a counter that the command keeps track of
         :param ran_range: range in which the command randomizes values (len = 2)
-        :param params:
-        :param name:
-        :param on_call:
+        :param params: does nothing yet
         """
         self.name = name
-        self.on_call = on_call
-        self.user_ids = user_ids
-        if count != None:
-            self.count = count
-        if ran_range:
-            self.ran_range = ran_range
-        if params:
-            self.params = params
+        self.message = message
+        if function: self.function = Command._parse_function(function)
+        else: self.function = Command._pass
+        self.count = count
+        self.ran_range = ran_range
+        Command.commands[self.name] = self
+
+    @staticmethod
+    def _pass():
+        pass
+
     def __call__(self, *args, **kwargs):
-        return self.on_call(self)
+        self.function()
+        if self.message: return self._parse_message()  #this is probably unsafe, i'll figure something out later since i'm the only one using the bot now
 
-counter = Command(['slenderkozak'],'counter', on_call=Command._counter, count=0)
+    def show(self):
+        print(self.name, self.message, self.count, self.function)
 
+    def _parse_function(func):
+        def function():
+            print('xd')
+        return function
+
+    def _parse_message(self):
+        return self.message
+
+
+# !add counter count=0 message="The count is {count}" function="count = count + 1"
+
+
+counter = Command(name='!counter', message='The count is {self.count}.format(self.count)', count=1, function = None)
+#counter.show()
+#print(counter())
+print(Command.add("add deth count=0 message='deth' function=None ran_range=(0,10)"))
+
+print(Command.commands["deth"].count)
